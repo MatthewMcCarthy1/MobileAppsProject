9f58364ab5c8aa4e7f44feb2378ed0bb828d7051
Matthew McCarthy <127790941+MatthewMcCarthy1@users.noreply.github.com>
2025-03-31 03:38:07 +0000

    added navigation to login page via login button

--- UniStack/src/app/main-load-in-page/main.page.ts
+++ UniStack/src/app/main-load-in-page/main.page.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
-import { Auth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: 'main.page.html',
   styleUrls: ['main.page.scss'],
+  standalone: true,
   imports: [IonContent],
 })
 export class MainPage {
-  private auth = inject(Auth); // Use Angular's inject function
+  private router = inject(Router);
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
