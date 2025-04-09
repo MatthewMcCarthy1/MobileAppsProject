c954b572115f39631bb7d16e740cbe6ca3cf69ba
Matthew McCarthy <127790941+MatthewMcCarthy1@users.noreply.github.com>
2025-04-09 18:33:58 +0000

    added routermodule to tabs

--- UniStack/src/pages/tabs/tabs.page.ts
+++ UniStack/src/pages/tabs/tabs.page.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
 import { home, addCircleOutline, personCircleOutline } from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, RouterModule]
 })
 export class TabsPage {
   constructor() {
