a4ccc3129ac60f1c69ea0f1d2a3fb152d10c588b
Matthew McCarthy <127790941+MatthewMcCarthy1@users.noreply.github.com>
2025-04-09 14:53:39 +0000

    routerlink added to homepage

--- UniStack/src/pages/home/home.page.ts
+++ UniStack/src/pages/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 import { addIcons } from 'ionicons';
 import { home, addCircleOutline, personCircleOutline } from 'ionicons/icons';
 
@@ -8,7 +9,7 @@ import { home, addCircleOutline, personCircleOutline } from 'ionicons/icons';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule]
+  imports: [IonicModule, RouterLink]
 })
 export class HomePage {
   constructor() {
