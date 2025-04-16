6f0107a0e97ca5852d7445c4d1373b238bbe3ba0
Matthew McCarthy <127790941+MatthewMcCarthy1@users.noreply.github.com>
2025-04-16 21:28:54 +0000

    set up pwa and service worker

--- UniStack/src/main.ts
+++ UniStack/src/main.ts
@@ -1,11 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, isDevMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
   enableProdMode();
@@ -16,6 +17,9 @@ bootstrapApplication(AppComponent, {
     ...appConfig.providers,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
   ]
 }).catch(err => console.error(err));
